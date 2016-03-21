6ae7e546deb3854a294dbcb9018140cb3940fc46
christine tacha and keir staple <keirstaple@comcast.net>
2016-03-21 11:46:36 -0700

    typescript setup

---
+++ app/sum.js
@@ -0,0 +1,14 @@
+// var number1: number;
+// var otherNumber: number;
+//
+// number1 = parseInt(prompt('please enter a number'));
+// otherNumber = parseInt(prompt('enter another number'));
+// var sum = number + otherNumber;
+// alert("the sume of your two numbers is:" + sum);
+var findSum = function (first, second) {
+    var sum = first + second;
+    alert("The sum of your two numbers is: " + sum);
+};
+var number1 = parseInt(prompt('please enter a number.'));
+var otherNumber = parseInt(prompt('enter another number'));
+findSum(number1, otherNumber);
